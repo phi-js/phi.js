0e9d971dc8bb62231272923476e502260efe0bbe
Santiago <santiago.cortes@phidias.com.co>
2021-09-20 18:00:41 +0200

    apis prototyping

---
+++ packages/phidias/apis/core/index.js
@@ -0,0 +1,6 @@
+export default {
+  type: 'phidias',
+  title: 'Encapsulador del API de phidias academico',
+  description: '*.phidias(.co|.es|.com.mx)/rest/',
+  docs: 'http://... url de documentacion ...'
+}
---
+++ packages/phidias/apis/v4/index.js
@@ -0,0 +1,8 @@
+export default {
+  type: 'v4',
+  title: 'Encapsulador del API de phidias v4',
+  description: '*.api.phidias.co',
+  docs: 'http://... url de documentacion ...'
+}
+
+export { default as people } from './people/index.js'
