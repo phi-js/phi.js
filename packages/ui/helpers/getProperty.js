14fc747c6c38ef3f8ecf9dd240ca4fa1c4e9477a
Santiago <santiago.cortes@phidias.com.co>
2021-09-24 17:33:56 +0200

    getProperty: handle "$." prefixes

--- packages/ui/helpers/getProperty.js
+++ packages/ui/helpers/getProperty.js
@@ -1,29 +1,37 @@
 // https://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key
 
 export default function getProperty(sourceObject, propertyName) {
+  if (propertyName == '$') {
+    return sourceObject
+  }
+
+  // remove trailing "$."
+  if (propertyName.substr(0, 2) == '$.') {
+    propertyName = propertyName.substr(2)
+  }
+
   if (!sourceObject || !propertyName) {
-    return undefined;
+    return undefined
   }
 
-  let s = propertyName;
-  let o = sourceObject;
+  let s = propertyName
+  let o = sourceObject
 
-  s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-  s = s.replace(/^\./, '');           // strip a leading dot
-  var a = s.split('.');
+  s = s.replace(/\[(\w+)\]/g, '.$1') // convert indexes to properties
+  s = s.replace(/^\./, '') // strip a leading dot
+  var a = s.split('.')
   for (var i = 0, n = a.length; i < n; ++i) {
-
     // Ocurre cuando  source:"{{ nombre.foo }}"   variables: {nombre: "yo no tengo subropiedades!"}
-    if (typeof o != "object" || o === null) {
-      return;
+    if (typeof o != 'object' || o === null) {
+      return
     }
 
-    var k = a[i];
+    var k = a[i]
     if (k in o) {
-      o = o[k];
+      o = o[k]
     } else {
-      return;
+      return
     }
   }
-  return o;
+  return o
 }
