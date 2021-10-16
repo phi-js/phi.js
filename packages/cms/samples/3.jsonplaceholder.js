de8f34d45d0c9c15c5b42fad93aea8e634a4eb0a
Santiago <santiago.cortes@phidias.com.co>
2021-10-16 20:52:57 +0200

    sample page

--- packages/cms/samples/3.jsonplaceholder.js
+++ packages/cms/samples/3.jsonplaceholder.js
@@ -1,147 +1,131 @@
 export default {
-  blocks: [
-    {
-      component: 'MediaHtml',
-      props: { value: '<h2>Hola {{ nombre }}!</h2>' },
-    },
-    // {
-    //   'component': 'InputText',
-    //   'props': { placeholder: 'Nombre aqui' },
-    //   'v-model': 'nombre',
-    // },
-    // {
-    //   component: 'MediaHtml',
-    //   props: { value: '<h2>Hola de nuevo {{ nombre }}!</h2>' },
-    //   // 'v-if': true,
-    // },
-    {
-      component: 'MediaHtml',
-      props: { value: '<h2>{{users.length}} usuarios</h2><pre>selectedUser: {{selectedUser}}</pre>' },
-      // 'v-if': {
-      //   and: [
-      //     {
-      //       field: 'users.length',
-      //       op: 'gte',
-      //       args: '{{ limit }}',
-      //     },
-      //   ],
-      // },
-    },
-    {
-      'component': 'MediaItem',
-      'props': {
-        icon: 'mdi:badge-account',
-        text: '{{$item.name}}',
-        subtext: '{{$item.email}}',
-
-        badge: {
-          if: {
-            field: 'selectedUser.id',
-            op: 'eq',
-            args: '{{$item.id}}',
-          },
-          then: '1',
-          else: null,
-        },
-        class: 'ui-clickable',
-      },
-      'v-for': 'users',
-      'v-on': {
-        click: {
-          chain: [
-            {
-              do: {
-                call: 'console.log',
-                args: ['Clicked user {{$item.id}}', '{{$item}}'],
-              },
-            },
-            {
-              do: {
-                if: {
-                  field: '$item.id',
-                  op: 'eq',
-                  args: '{{selectedUser.id}}',
-                },
-                then: {
-                  chain: [{
-                    do: null,
-                    assign: 'selectedUser',
-                  } ],
-                },
-                else: {
-                  chain: [{
-                    do: '{{$item}}',
-                    assign: 'selectedUser',
-                  } ],
-                },
-              },
-            },
-            {
-              // do: '{{selectedUser.name}}',
-              do: '{{$item.name}}',
-              assign: 'nombre',
-            },
-          ],
-        },
-      },
-    },
-    // {
-    //   'component': 'MediaHtml',
-    //   'props': {
-    //     value: '<li><strong>{{$item.title}}</strong></li>',
-    //     class: 'ui-clickable',
-    //   },
-    //   'v-for': 'posts',
-    // },
-    {
-      component: 'MediaLoremIpsum',
-      props: {
-        nParagraphs: '{{ cantidad }}',
-        nWords: '{{ palabras }}',
-      },
-    },
-    {
-      'component': 'InputButton',
-      'props': { label: 'Test' },
-      'v-on': {
-        click: {
-          chain: [
-            {
-              do: 'damn yu',
-              assign: 'nombre',
-            },
-            {
-              do: 'damn yu again',
-              assign: 'nombre',
-            },
-            {
-              do: 'damn yu again 2',
-              assign: 'nombre',
-            },
-          ],
-        },
-      },
-    },
-  ],
-
+  theme: 'default',
   setup: {
     chain: [
       {
-        info: { text: 'fetch users', icon: 'mdi:plus-network' },
+        info: {
+          text: 'fetch users',
+          icon: 'mdi:plus-network',
+        },
         do: {
           call: 'fetch',
           args: { url: 'https://jsonplaceholder.typicode.com/users' },
         },
         assign: 'users',
       },
-      {
-        info: { text: 'fetch posts', icon: 'mdi:plus-network' },
-        do: {
-          call: 'fetch',
-          args: { url: 'https://jsonplaceholder.typicode.com/posts' },
-        },
-        assign: 'posts',
-      },
     ],
   },
+  sections: [
+    {
+      id: 'section-0',
+      class: '',
+      rows: [
+        {
+          columns: [
+            {
+              blocks: [
+                {
+                  'component': 'MediaItem',
+                  'props': {
+                    icon: 'mdi:badge-account',
+                    text: '{{$item.name}}',
+                    subtext: '{{$item.email}}',
+                    badge: {
+                      if: {
+                        field: 'selectedUser.id',
+                        op: 'eq',
+                        args: '{{$item.id}}',
+                      },
+                      then: '1',
+                      else: null,
+                    },
+                    class: 'ui-clickable',
+                  },
+                  'v-for': 'users',
+                  'v-on': {
+                    click: {
+                      chain: [
+                        {
+                          do: {
+                            call: 'console.log',
+                            args: [
+                              'Clicked user {{$item.id}}',
+                              '{{$item}}',
+                            ],
+                          },
+                        },
+                        {
+                          do: {
+                            if: {
+                              field: '$item.id',
+                              op: 'eq',
+                              args: '{{selectedUser.id}}',
+                            },
+                            then: {
+                              chain: [
+                                {
+                                  do: null,
+                                  assign: 'selectedUser',
+                                },
+                              ],
+                            },
+                            else: {
+                              chain: [
+                                {
+                                  do: '{{$item}}',
+                                  assign: 'selectedUser',
+                                },
+                              ],
+                            },
+                          },
+                        },
+                        {
+                          do: '{{$item.name}}',
+                          assign: 'nombre',
+                        },
+                      ],
+                    },
+                  },
+                  'id': 'block-2',
+                  'class': '',
+                },
+              ],
+              id: 'col-0',
+              flex: 384,
+              title: 'Column 1',
+              class: '',
+              initialFlex: 323,
+            },
+            {
+              id: 0,
+              title: 'new column',
+              flex: 913,
+              class: '',
+              blocks: [
+                {
+                  component: 'MediaHtml',
+                  props: { value: '<p>{{users.length}} usuarios</p><h1>{{selectedUser.name}}</h1><pre><code>{{selectedUser}}</code></pre>' },
+                  id: 'block-1',
+                  class: '',
+                },
+                {
+                  component: 'MediaLoremIpsum',
+                  props: {
+                    nParagraphs: '{{ cantidad }}',
+                    nWords: '{{ palabras }}',
+                  },
+                  id: 'block-3',
+                  class: '',
+                },
+              ],
+              initialFlex: 974,
+            },
+          ],
+          id: 'row-0',
+          title: 'Row 1',
+          class: '',
+        },
+      ],
+    },
+  ],
 }
