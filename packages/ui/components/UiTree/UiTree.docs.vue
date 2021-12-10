be7497449a3ad6cd7d67f72483300dc5e45bb838
Santiago <santiago.cortes@phidias.com.co>
2021-12-10 20:42:48 +0100

    refactored UiTree

--- packages/ui/components/UiTree/UiTree.docs.vue
+++ packages/ui/components/UiTree/UiTree.docs.vue
@@ -3,6 +3,7 @@ const tree = [
   {
     text: 'Un elemento',
     subtext: 'Un subtexto',
+    color: 'red',
     children: [
       {
         text: 'Hijo 1',
@@ -32,10 +33,12 @@ const tree = [
   {
     text: 'Otro elemento',
     subtext: 'Otro subtexto (sin hijos)',
+    color: 'blue',
   },
   {
     text: 'Otro elemento',
     subtext: 'Otro subtexto',
+    color: 'purple',
     children: [
       { text: 'Hijo 1' },
       {
@@ -59,12 +62,55 @@ const tree = [
 ]
 
 import { UiTree } from './index.js'
+
+import { UiItem, UiIcon } from '../'
 </script>
 
 <template>
   <h1>UiTree</h1>
   <UiTree
+    v-slot="{item, children, isOpen, toggle}"
+    class="myTree"
     :value="tree"
-    style="max-width: 300px"
-  />
+  >
+    <UiItem
+      v-bind="item"
+      class="myTreeItem ui--clickable"
+      :class="{'myTreeItem--open': isOpen}"
+      :style="{'--item-color': item.color}"
+      @click="toggle"
+    >
+      <template
+        v-if="children?.length"
+        #actions
+      >
+        <UiIcon :src="isOpen ? 'mdi:chevron-down' : 'mdi:chevron-right'" />
+      </template>
+    </UiItem>
+  </UiTree>
 </template>
+
+<style lang="scss">
+.myTree {
+  max-width: 400px;
+
+  .UiTree {
+    // &__face {
+    // }
+
+    &__children {
+      margin-left: 12px;
+    }
+  }
+}
+
+.myTreeItem {
+  border-radius: var(--ui-radius);
+  transition: all var(--ui-duration-snap);
+
+  &--open {
+    background-color: var(--item-color, #999) !important;
+    color: #fff;
+  }
+}
+</style>
