a4a6b9da3a65fec3b2b468b72cc0de23e4c936c5
Santiago <santiago.cortes@phidias.com.co>
2021-12-15 00:36:09 +0100

    test: localStorage story for homepage

--- pages/1.home.vue
+++ pages/1.home.vue
@@ -1,9 +1,52 @@
+<script setup>
+import { ref } from 'vue'
+import { CmsStoryEditor } from '/packages/cms'
+
+const defaultStory = {
+  pages: [
+    {
+      component: 'LayoutPage',
+      slot: [
+        {
+          component: 'LayoutRow',
+          props: { style: { textAlign: 'center' } },
+          slot: [
+            {
+              component: 'LayoutColumn',
+              slot: [
+                {
+                  component: 'MediaImage',
+                  props: {
+                    alt: 'Phi.js',
+                    title: 'Hello world!',
+                    src: '/phi.png',
+                  },
+                },
+                {
+                  component: 'MediaHtml',
+                  props: { value: '<h1>Phi.js</h1>' },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
+const savedStory = JSON.parse(localStorage.getItem('phijs:home'))
+const story = ref(savedStory || defaultStory)
+
+function saveStory() {
+  localStorage.setItem('phijs:home', JSON.stringify(story.value))
+}
+
+</script>
+
 <template>
-  <div style="text-align: center">
-    <img
-      alt="Phi.js"
-      src="/phi.png"
-    >
-    <h1>Phi.js</h1>
-  </div>
+  <CmsStoryEditor
+    v-model:story="story"
+    @update:story="saveStory()"
+  />
 </template>
