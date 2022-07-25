18ec852b221baebf5c91e3190f38825580dd51d2
Santiago <santiago.cortes@phidias.com.co>
2022-07-25 17:45:24 +0200

    ecommerce plugin factory

--- packages/ecommerce/plugin/index.js
+++ packages/ecommerce/plugin/index.js
@@ -1,14 +1,16 @@
 import EcommerceInvoice from '../components/EcommerceInvoice/EcommerceInvoice.block.js'
 import EcommercePaymentButton from '../components/EcommercePaymentButton/EcommercePaymentButton.block.js'
 
-export default {
-  blocks: [
-    {
-      title: 'eCommerce',
-      children: [
-        EcommerceInvoice,
-        EcommercePaymentButton,
-      ],
-    },
-  ],
+export default function() {
+  return {
+    blocks: [
+      {
+        title: 'eCommerce',
+        children: [
+          EcommerceInvoice,
+          EcommercePaymentButton,
+        ],
+      },
+    ],
+  }
 }
