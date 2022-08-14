bc5c8e333c4d336e10334de4a4a1c2e4a90935bd
Santiago <santiago.cortes@phidias.com.co>
2022-08-14 12:34:16 +0200

    lazy load UiIcon

--- packages/ui/components/UiIcon/providers.js
+++ packages/ui/components/UiIcon/providers.js
@@ -1,5 +1,7 @@
+import { defineAsyncComponent } from 'vue'
+// import ProviderMdi from './Provider/Mdi.vue'
+const ProviderMdi = defineAsyncComponent(() => import('./Provider/Mdi.vue'))
 import ProviderImage from './Provider/Image.vue'
-import ProviderMdi from './Provider/Mdi.vue'
 import ProviderText from './Provider/Text.vue'
 
 export default {
@@ -8,5 +10,5 @@ export default {
   https: ProviderImage,
   data: ProviderImage,
   mdi: ProviderMdi,
-  text: ProviderText
+  text: ProviderText,
 }
--- packages/ui/components/UiIconPicker/index.js
+++ packages/ui/components/UiIconPicker/index.js
@@ -1,1 +1,3 @@
-export { default as UiIconPicker } from './UiIconPicker.vue'
+// export { default as UiIconPicker } from './UiIconPicker.vue'
+import { defineAsyncComponent } from 'vue'
+export const UiIconPicker = defineAsyncComponent(() => import('./UiIconPicker.vue'))
