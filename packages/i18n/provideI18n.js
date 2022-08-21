929203e600f457277aa4c3133a0063b6f12f92e4
Santiago <santiago.cortes@phidias.com.co>
2022-08-21 10:52:47 +0200

    set locale on provideI18n

--- packages/i18n/provideI18n.js
+++ packages/i18n/provideI18n.js
@@ -1,8 +1,13 @@
 import { provide, reactive } from 'vue'
+import { locale } from './locale'
 
 export default function provideI18n(options) {
   const provided = reactive(options)
   provide('$_phi_i18n', provided)
+
+  if (options?.locale) {
+    locale.value = options.locale
+  }
 
   return provided
 }
