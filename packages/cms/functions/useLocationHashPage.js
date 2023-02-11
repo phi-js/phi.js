98622801562443a9372aa96e901ad93675b22695
Santiago <santiago.cortes@phidias.com.co>
2023-02-11 23:26:41 +0100

    useLocationHashPage improved prefix handling

--- packages/cms/functions/useLocationHashPage.js
+++ packages/cms/functions/useLocationHashPage.js
@@ -1,26 +1,39 @@
 import { ref, computed } from 'vue'
 
-export default function useLocationHashPage(prefix = '') {
+export default function useLocationHashPage(prefix = '#p:') {
   const hash = ref('')
 
   function onHashChange() {
-    hash.value = window.location.hash
-      .replace(prefix, '')
-      .replace('#', '')
+    const foundPageId = extractPageId(window.location.hash, prefix)
+    if (foundPageId) {
+      hash.value = foundPageId
+    }
   }
 
-  // initial et
-  onHashChange()
-
   window.addEventListener('hashchange', onHashChange)
+  onHashChange() // initial run
 
   return computed({
     get() {
       return hash.value
     },
 
-    set(newValue) {
-      window.location.hash = prefix + '#' + newValue
+    set(newPageId) {
+      window.location.hash = hash.value // hash.value is the "old" pageId value
+        ? window.location.hash.replace(`${prefix}${hash.value}`, '') + `${prefix}${newPageId}`
+        : window.location.hash + `${prefix}${newPageId}`
     },
   })
+}
+
+// str: '#/some/page/hash/-#p:hola'
+// prefix: '#p'
+// ---> 'hola'
+// (by chatGPT)
+function extractPageId(str, prefix) {
+  if (!str.includes(prefix)) {
+    return null
+  }
+  const startIndex = str.indexOf(prefix) + prefix.length
+  return str.substring(startIndex)
 }
