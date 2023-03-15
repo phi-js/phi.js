9509a71e4275337e7feede7c6f3a37c3cff325be
Santiago <santiago.cortes@phidias.com.co>
2023-03-15 11:18:33 +0100

    provide 'phi' http client

--- dev/index.js
+++ dev/index.js
@@ -1,3 +1,7 @@
 import { createApp } from 'vue'
+
+import { provideClient } from '../packages/api'
+provideClient('phi', { baseURL: 'http://phi.local/' })
+
 import App from './App.vue'
 createApp(App).mount('#app')
