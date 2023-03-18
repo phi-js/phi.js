134b3a2cbea522e4cf689d4150ef5e67bd8a6d99
Santiago <santiago.cortes@phidias.com.co>
2023-03-18 12:30:57 +0100

    BlockCssEditor: relocate AI assistant

--- packages/cms/components/CmsBlockEditor/BlockCssEditor.vue
+++ packages/cms/components/CmsBlockEditor/BlockCssEditor.vue
@@ -3,8 +3,6 @@ import { UiTabs, UiTab } from '@/packages/ui'
 import BlockCssClasses from './BlockCssClasses.vue'
 import BlockCssStyle from './BlockCssStyle.vue'
 
-import OpenAiCss from '../../plugins/openai/OpenAiCss.vue'
-
 const props = defineProps({
   /**
    * @model Block object
@@ -15,32 +13,6 @@ const props = defineProps({
   },
 })
 const emit = defineEmits(['update:modelValue'])
-
-/**
- * Deep merge two objects.
- * @param target
- * @param ...sources
- */
-function mergeDeep(target, ...sources) {
-  if (!sources.length) return target
-  const source = sources.shift()
-
-  if (isObject(target) && isObject(source)) {
-    for (const key in source) {
-      if (isObject(source[key])) {
-        if (!target[key]) Object.assign(target, { [key]: {} })
-        mergeDeep(target[key], source[key])
-      } else {
-        Object.assign(target, { [key]: source[key] })
-      }
-    }
-  }
-  return mergeDeep(target, ...sources)
-}
-
-function isObject(item) {
-  return (item && typeof item === 'object' && !Array.isArray(item))
-}
 </script>
 
 <template>
@@ -50,13 +22,6 @@ function isObject(item) {
   >
     <slot name="default" />
 
-    <UiTab text="Classes">
-      <BlockCssClasses
-        :block="props.modelValue"
-        @update:block="emit('update:modelValue', $event)"
-      />
-    </UiTab>
-
     <UiTab text="Properties">
       <BlockCssStyle
         :block="props.modelValue"
@@ -64,10 +29,10 @@ function isObject(item) {
       />
     </UiTab>
 
-    <UiTab text="OpenAI">
-      <OpenAiCss
-        :existing="props.modelValue.props?.style"
-        @input="emit('update:modelValue', mergeDeep(props.modelValue, {props:{style:$event}}))"
+    <UiTab text="Classes">
+      <BlockCssClasses
+        :block="props.modelValue"
+        @update:block="emit('update:modelValue', $event)"
       />
     </UiTab>
   </UiTabs>
