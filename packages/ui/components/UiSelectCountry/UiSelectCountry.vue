4480f3348099195010d3e2288e2274802d558fd4
Santiago <santiago.cortes@phidias.com.co>
2023-05-24 20:05:28 +0200

    UiSelectCountry: fallback language PT language

--- packages/ui/components/UiSelectCountry/UiSelectCountry.vue
+++ packages/ui/components/UiSelectCountry/UiSelectCountry.vue
@@ -7,7 +7,17 @@ const i18n = useI18n()
 const countryOptions = ref([])
 
 watchEffect(async () => {
-  const imported = await import(`./data/${i18n.baseLanguage.value}/country.json`)
+  let imported = null
+  try {
+    imported = await import(`./data/${i18n.baseLanguage.value}/country.json`)
+  } catch (e) {
+    imported = await import('./data/en/country.json')
+  }
+
+  if (!imported?.default) {
+    return
+  }
+
   countryOptions.value = Object.keys(imported.default)
     .map((countryCode) => {
       return {
