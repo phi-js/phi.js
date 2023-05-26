407d71da2356cd5b328a2c6fb91e152f5298cb1d
Santiago <santiago.cortes@phidias.com.co>
2023-05-26 21:59:49 +0200

    enum.any bugfix

--- packages/vm/lib/operators/enum.js
+++ packages/vm/lib/operators/enum.js
@@ -1,13 +1,30 @@
 export default {
   'enum.any'(value, args) {
-    if (!Array.isArray(args) || !value?.includes) {
-      return false
+    if (Array.isArray(value)) {
+      return arrayIncludesAny(value, args)
     }
-    for (let i = 0; i < args.length; i++) {
-      if (value.includes(args[i])) {
-        return true
-      }
-    }
-    return false
+
+    return stringIncludesAny(value, args)
   },
+}
+
+
+function stringIncludesAny(str, args) {
+  if (!Array.isArray(args)) {
+    return false
+  }
+  return args.includes(str)
+}
+
+function arrayIncludesAny(arr, args) {
+  if (!Array.isArray(args)) {
+    return false
+  }
+
+  for (let i = 0; i < args.length; i++) {
+    if (arr.includes(args[i])) {
+      return true
+    }
+  }
+  return false
 }
