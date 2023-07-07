4ff80428150deff4b0c1cd91d9921857b208ef36
Santiago <santiago.cortes@phidias.com.co>
2023-07-07 12:12:47 +0200

    main package exports

--- packages/ui/index.js
+++ packages/ui/index.js
@@ -1,5 +1,7 @@
 export * from './components'
 export * from './color-scheme'
 
+export { default as normalize } from './helpers/normalize.js'
+
 export const UiFileGallery = null
 export const UiFileUploader = null
