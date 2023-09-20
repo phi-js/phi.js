e2fa1bc80b2eab40ef2d5c6202cca976b2f7cf30
sebastian cordoba <sebastian.cordoba@phidias.com.co>
2023-09-20 09:59:14 -0500

    UiScrollContainer

---
+++ packages/ui/components/UiScrollContainer/UiScrollContainer.docs.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue"
+import UiScrollContainer from "./UiScrollContainer.vue";
+
+const loader = ref(true);
+const counter = ref(100);
+
+const handleScroll = () => {
+  if (counter.value >= 500) {
+    loader.value = false;
+    return;
+  }
+  setTimeout(() => counter.value += 100, 600);
+}
+</script>
+
+<template>
+  <div>
+    <h1>Documentación "UiScrollContainer"</h1>
+    <hr>
+    <UiScrollContainer @scroll-bottom="handleScroll" style="height: 200px;">
+      <ul>
+        <li v-for="n in counter">Item {{ n }}</li>
+      </ul>
+      <div v-if="loader">..cargando...</div>
+    </UiScrollContainer>
+  </div>
+</template>
---
+++ packages/ui/components/UiScrollContainer/UiScrollContainer.vue
@@ -0,0 +1,22 @@
+<script setup>
+const emit = defineEmits(['scroll-bottom'])
+
+const handleScroll = (event) => {
+  const scrollableDiv = event.target;
+  if (scrollableDiv.scrollTop + scrollableDiv.clientHeight >= scrollableDiv.scrollHeight) {
+    emit('scroll-bottom');
+  }
+}
+</script>
+
+<template>
+  <div class="UiScrollContainer" @scroll="handleScroll($event)">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss">
+.UiScrollContainer {
+  overflow: auto;
+}
+</style>
